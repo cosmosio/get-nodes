517c03fca65acfdfaec6dd2cf041ef4bb74ef00f
Olivier Scherrer <pode.fr@gmail.com>
2014-04-12 11:56:03 -0400

    add implementation

--- index.js
+++ index.js
@@ -6,3 +6,9 @@
 * Copyright (c) 2014 Olivier Scherrer <pode.fr@gmail.com>
 */
 "use strict";
+
+var toArray = require("to-array");
+
+module.exports = function getNodes(dom) {
+    return toArray(dom.querySelectorAll("*")).unshift(dom);
+};
