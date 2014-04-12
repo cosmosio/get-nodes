b7e3fb583bd68af0fcd19e68d1c747f7121ab9ba
Olivier Scherrer <pode.fr@gmail.com>
2014-04-12 11:38:42 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+* @license get-nodes https://github.com/cosmios/get-nodes
+*
+* The MIT License (MIT)
+*
+* Copyright (c) 2014 Olivier Scherrer <pode.fr@gmail.com>
+*/
+"use strict";
---
+++ test/get-nodes.spec.js
@@ -0,0 +1,7 @@
+/**
+* @license get-nodes https://github.com/cosmios/get-nodes
+*
+* The MIT License (MIT)
+*
+* Copyright (c) 2014 Olivier Scherrer <pode.fr@gmail.com>
+*/
